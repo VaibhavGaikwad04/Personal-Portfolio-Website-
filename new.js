1e92c875adb231a92f01e88b9ca3b4451c5a6c89
VaibhavGaikwad04 <115126278+VaibhavGaikwad04@users.noreply.github.com>
2022-10-29 09:12:47 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,20 @@
+
+ document.querySelector(".list").style.backgroundColor="blue";
+
+ document.querySelector("p").style.backgroundColor="green";
+
+ document.querySelector("body").style.backgroundColor="orange";
+
+ document.querySelector("#sls").style.height="100px";
+ document.querySelector("#sls").style.width="400px";
+
+ document.querySelector("#sls").style.top="500px";
+ document.querySelector("#sls").style.left="10px";
+ document.querySelector("#sls").style.backgroundColor.hover="green";
+ document.querySelector("h1").style.backgroundColor="yellow";
+ document.querySelector("h1").style.height="100px";
+ document.querySelector("h1").style.width="50px";
+
+
+
+   
